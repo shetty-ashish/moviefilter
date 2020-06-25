2e22815ad047d50e3666e713fa7780d698cbb52c
shetty-ashish <iashish.shetty@gmail.com>
2020-06-25 21:32:47 +0200

    Updates

---
+++ precache-manifest.fc99a0ae808a28ca01f2556c8031bc87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d49662cfb7818fff5bef34bbea6c97",
+    "url": "/shetty-ashish/moviefilter/index.html"
+  },
+  {
+    "revision": "db0511b8f41129c95800",
+    "url": "/shetty-ashish/moviefilter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86b6827c4cedb35591c2",
+    "url": "/shetty-ashish/moviefilter/static/js/2.445fd0ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shetty-ashish/moviefilter/static/js/2.445fd0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0511b8f41129c95800",
+    "url": "/shetty-ashish/moviefilter/static/js/main.c71bdf6b.chunk.js"
+  },
+  {
+    "revision": "48ecd8e5aa11b1379ff4",
+    "url": "/shetty-ashish/moviefilter/static/js/runtime-main.3b23e860.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shetty-ashish/moviefilter/static/media/logo.5d5d9eef.svg"
+  }
+]);
